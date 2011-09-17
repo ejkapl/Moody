082bf08e621b9fc9320888b0ea65cc06b00b8d7e
Eliot <kaplane@seas.upenn.edu>
2011-09-17 18:09:44 -0400

    Updated the .CAT file to include the NONEMOTIONS category.

--- categorize.js
+++ categorize.js
@@ -1,24 +1,40 @@
+var primaryCats = {
+	NEED:["ORALITY","ANALITY","SEX"],
+	SENSATION:["TOUCH","TASTE","ODOR","GEN_SENSATION","SOUND","VISION","COLD","HARD","SOFT"],
+	DEFENSIVE_SYMBOL:["PASSIVITY","VOYAGE","RANDOM MOVEMENT","DIFFUSION","CHAOS"],
+	REGR_KNOL:["UNKNOW","TIMELESSNES","COUNSCIOUS","BRINK-PASSAGE","NARCISSISM","CONCRETENESS"],
+	ICARIAN_IM:["ASCEND","HEIGHT","DESCENT","DEPTH","FIRE","WATER"]
+};
+var secondaryCats = {
+	NONEMOTIONS:["ABSTRACT_TOUGHT","SOCIAL_BEHAVIOR","INSTRU_BEHAVIOR","RESTRAINT","ORDER","TEMPORAL_REPERE","MORAL_IMPERATIVE"],
+	EMOTIONS:["POSITIVE_AFFECT","ANXIETY","SADNESS","AFFECTION","AGGRESSION","EXPRESSIVE_BEH","GLORY"]
+};
+var categories = {
+	PRIMARY:primaryCats,
+	SECONDARY:secondaryCats
+};
+	
+
+
 function wordCounts(str){
-	str = str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
-	str = str.toLowerCase();
-	words = str.split(" ");
+	str = str.replace(/[^\w\s]|_/g, "");
+	str = str.toUpperCase();
+	words = str.split(/\s+/g);
 	var catCount = {};
 	for (word in words){
 		var cats = getCat(words[word]);
-		for (cat in cats){
-			if (catCount[cats[cat]] == undefined){
-				catCount[cats[cat]] = 0;
+		if (cats){
+			for (cat in cats){
+				if (catCount[cats[cat]] == undefined){
+					catCount[cats[cat]] = 0;
+				}
+				catCount[cats[cat]]++;
 			}
-			catCount[cats[cat]]++;
 		}
 	}
 	return catCount;
 }
 
-function getCat(word){
-	//Look up the word (a string) in the DB and return an array of the categories under which the word falls
-	var category1 = "category";
-	var category2 = "subcategory";
-	var category3 = "subsubcategory";
-	return [category1, category2, category3];
-}
+//getCategories:
+	//IN: String word
+	//ARRAY OF CATEGORIES
