799b4cf618d81bafe758ae96ec5b2498f3656cc0
unknown <gmenezes@.(none)>
2011-09-17 21:16:30 -0400

    working tab + close + graph

--- scripts/injection.js
+++ scripts/injection.js
@@ -1,7 +1,14 @@
 //var $j = jQuery.noConflict();
 
-var hash = window.location.hash;
-//alert(hash);
+
+// Bind an event to window.onhashchange that, when the hash changes, gets the
+// hash and adds the class "selected" to any matching nav link.
+$(window).hashchange(createPanel);
+
+// Since the event is only triggered when the hash changes, we need to trigger
+// the event now, to handle the hash the page may have loaded with.
+$(window).hashchange();
+
 
 function injectHtml(element, html){
   $(element).prepend(html);
@@ -9,22 +16,25 @@ function injectHtml(element, html){
 function outjectHtml(id){
   $(id).remove();
 }
-
-// Bind an event to window.onhashchange that, when the hash changes, gets the
-// hash and adds the class "selected" to any matching nav link.
-$(window).hashchange(function(){
-  var hash = location.hash;
+function createPanel(){
+  var hash = window.location.hash;
   
   if(hash.indexOf("#compose") > -1 || hash.indexOf("#drafts/") > -1){
     outjectHtml("#panel");
-    injectHtml('body', '<div id="panel"><!-- you can put content here --></div>');
-    $('head').append('<script>$("body").load(function() {$("#panel").slideDown("slow");});</script>');
-  }else{
+    //injectHtml('head',
+    //'<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>');
+    //injectHtml('body',
+    //'<script> var $j = jQuery.noConflict(); alert(\"THIS IS WORKING\")"</script>');
+    injectHtml('body',
+    '<div id="panel">' +
+    '<canvas id="chartCanvas5" width="300%" height="300%">Your web-browser does not support the HTML 5 canvas element.</canvas>' +
+    '<div id="close_panel" onclick="var element=document.getElementById(\'panel\'); element.parentNode.removeChild(element);">CLICK ME!</div>' +
+    '</div>');
+    drawChart();
+    openPanel();
+  } else{
+    //closePanel();
     outjectHtml("#panel");
   }
   // Iterate over all nav links, setting the "selected" class as-appropriate.
-})
-
-// Since the event is only triggered when the hash changes, we need to trigger
-// the event now, to handle the hash the page may have loaded with.
-$(window).hashchange();
+}
--- scripts/toolbar.js
+++ scripts/toolbar.js
@@ -1,3 +1,17 @@
-$('body').load(function() {
-    $('#panel').slideDown('slow');
-});
+function openPanel(){
+  setTimeout(function() {$('#panel').animate({height: "18em"}, "slow")} , 1000);
+}
+
+function closePanel(){
+  $('#panel').animate({height: "18em"}, "slow").delay(500).animate({height: "0em"}, "slow");
+}
+
+function drawChart(){
+  var chart5 = new AwesomeChart('chartCanvas5');
+  chart5.chartType = "pie";
+  //chart5.title = "Worldwide browser market share: December 2010";
+  chart5.data = [51.62,31.3,10.06,4.27,1.96,0.78];
+  chart5.labels = ['IE','Firefox','Chrome','Safari','Opera','Other'];
+  chart5.colors = ['#006CFF', '#FF6600', '#34A038', '#945D59', '#93BBF4', '#F493B8'];
+  chart5.draw();
+}
