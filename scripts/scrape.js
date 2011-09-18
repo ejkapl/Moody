3ef3d49cbf89d5a76c827b7ed15551a5fe1a48c2
unknown <gmenezes@.(none)>
2011-09-18 03:10:49 -0400

    committing working pie chart (not yet gmail)

---
+++ scripts/scrape.js
@@ -0,0 +1,39 @@
+var ul = document.getElementsByTagName( "div" );
+var arr;
+var para = null;
+
+function getGDocText() {
+	arr = getElementsByClassName(document, "div", "kix-lineview-content");
+
+	var text = '';
+	for(var j = 0; j < arr.length; j++)
+	{
+		elems = arr[j].getElementsByTagName("span");
+		for(var i = 0; i < elems.length; i++)
+		{
+			elem = elems[i];
+			if(elem.className == '') {
+			//alert("help");
+				text = text + elem.textContent + '\n';
+			}
+			
+		}
+	}
+	return text;
+}
+
+function getElementsByClassName(doc, strTagName, strClassName){
+
+	var arrElements = (strTagName == "*" && document.all)? document.all : doc.getElementsByTagName(strTagName);
+	var arrReturnElements = new Array();
+	strClassName = strClassName.replace(/\-/g, "\\-");
+	var oRegExp = new RegExp("(^|\\s)" + strClassName + "(\\s|$)");
+	var oElement;
+	for(var i=0; i<arrElements.length; i++){
+		oElement = arrElements[i];
+		if(oRegExp.test(oElement.className)){
+			arrReturnElements.push(oElement);
+		}
+	}
+	return (arrReturnElements)
+}
